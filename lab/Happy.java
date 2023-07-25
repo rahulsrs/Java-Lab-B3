e53022d42e1c6c4e737dc4075699a3ee690b22f3
rahulsrs <rahulacharya2003@gmail.com>
2023-07-25 07:50:23 +0530

    happyno

--- lab/Happy.java
+++ lab/Happy.java
@@ -1,1 +1,36 @@
-39
+package lab;
+import java.util.*;
+interface yes{
+    int check(int num);
+}
+
+class Happy_no implements yes {
+    public int check(int num) {
+        int rem = 0, sum = 0;
+        // calculate the sum of squares of each digit
+        while (num > 0) {
+            rem = num % 10;
+            sum = sum + (rem * rem);
+            num = num / 10;
+        }
+        return sum;
+    }}
+    
+    public class Happy{
+    public static void main(String[] args) {
+        // Take num
+        Happy_no obj = new Happy_no();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number greater than 0:");
+        int num = sc.nextInt();
+        int finalVal = num;
+        while (finalVal != 1 && finalVal != 4) {
+            finalVal = obj.check(finalVal);
+        }
+        if (finalVal == 1) {
+            System.out.println("The number entered is a Happy Number");
+        } else {
+            System.out.println(" The number entered is not a Happy Number");
+        }
+    }
+}
